0480ea0d29bab9f75cb018dadbca8c42d2dc4f54
Jarrett Cruger <jcrugzz@gmail.com>
2013-08-18 23:39:05 -0400

    [init] commit things

---
+++ index.js
@@ -0,0 +1,17 @@
+/*
+ * index.js :: include for the binarypack modules
+ *
+ * (C) 2013 Jarrett Cruger -- MIT
+ *
+ */
+
+var fs = require('fs');
+
+module.exports = require('binarypack');
+
+//
+// Expose source of browser code to be loaded by primus
+//
+module.exports.browser = fs.readFileSync('./vendor/binarypack.js', 'utf-8');
+
+
